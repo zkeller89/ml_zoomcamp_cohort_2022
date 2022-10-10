9d27fd6918fa5b019b73bd448cc5354ea5c0c7c5
Zack <keller.zr@gmail.com>
2022-10-10 18:53:19 +0000

    update to use model2 in docker app

---
+++ week_5/homework/predict_docker.py
@@ -0,0 +1,35 @@
+import pickle
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+model_file = 'model2.bin'
+dv_file = 'dv.bin'
+
+with open(model_file, 'rb') as f_in:
+    model = pickle.load(f_in)
+
+with open(dv_file, 'rb') as f_in:
+    dv = pickle.load(f_in)
+
+app = Flask('cc')
+
+@app.route('/predict_docker', methods=['POST'])
+def predict():
+    client = request.get_json()
+
+    X = dv.transform([client])
+    y_pred = model.predict_proba(X)[0, 1]
+    churn = y_pred >= 0.5
+
+    result = {
+        'get_cc_probability': float(y_pred),
+        'get_cc': bool(churn)
+    }
+
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
